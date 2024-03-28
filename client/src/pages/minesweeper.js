b1e782e18c3e81f90f16ca6c45ee27723d942386
Alf <alfgoto@gmail.com>
2024-03-28 11:25:54 +0100

    grid colors done

--- client/src/pages/minesweeper.js
+++ client/src/pages/minesweeper.js
@@ -7,19 +7,52 @@ function Minesweeper() {
 
     useEffect(() => {
         const socket = io.connect('http://localhost:4000')
-        socket.on('width', msg => { 
-            width = msg 
-            console.log(width)
-            for(let i = 0; i< width*width; i++){
+
+
+
+        socket.on('width', msg => {
+            document.getElementById('grid').innerHTML = ''
+            width = msg
+            for (let i = 0; i < width * width; i++) {
                 let square = document.createElement('div')
                 square.classList.add('square')
+                square.id = i
                 document.getElementById('grid').appendChild(square)
+
+                if (Math.ceil(i + 1 / width) % 2 == 0) {
+                    // square.classList.add(Math.ceil(i/width))
+                    if (Math.ceil(i / width) % 2 == 0) {
+                        square.classList.add('dark')
+                    } else {
+                        square.classList.add('light')
+                    }
+                } else {
+                    if (i % width == 0) {
+                        square.classList.add(Math.ceil(i / width))
+                        if (Math.ceil(i / width) % 2 == 0) {
+                            square.classList.add('dark')
+                        } else {
+                            square.classList.add('light')
+                        }
+                    } else {
+                        square.classList.add(Math.ceil(i / width))
+                        if (Math.ceil(i / width) % 2 == 0) {
+                            square.classList.add('light')
+                        } else {
+                            square.classList.add('dark')
+                        }
+                    }
+                }
+
+                square.addEventListener('click', () => {
+                    socket.emit('click', i)
+                })
             }
         })
 
         let grid = document.getElementById('grid')
     }, [])
-    
+
 
 
     return (
